063174d81c439e41985045503b3ce34179ca6477
Simon Llanso <cogliostro_g@hotmail.com>
2016-03-29 16:54:21 +0200

    Test 2

--- src/Inbetween/Startup.cs
+++ src/Inbetween/Startup.cs
@@ -12,6 +12,7 @@ namespace Inbetween
     public class Startup
     {
         // This is a test Kostas!
+        // This is another test Mikael!
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
