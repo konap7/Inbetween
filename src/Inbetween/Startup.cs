5373c817a3bf6c9baaf869ee8e221388586ad9cb
Mikael Brunnberg <mikael.brunnberg@gmail.com>
2016-03-30 09:32:03 +0200

    Project.json och Startup.cs uppdaterad

--- src/Inbetween/Startup.cs
+++ src/Inbetween/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Data.Entity;
+//using CatShelter.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Inbetween
 {
@@ -17,17 +20,22 @@ namespace Inbetween
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
+            app.UseCookieAuthentication(o =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                o.AutomaticChallenge = true;
+                //o.LogoutPath = new PathString("/Members/Login");
             });
+
+            app.UseStaticFiles();
+            app.UseDeveloperExceptionPage();
+            app.UseIdentity();
+            app.UseMvcWithDefaultRoute();
         }
 
         // Entry point for the application.
