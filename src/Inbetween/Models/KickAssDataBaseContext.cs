76ea682741c70b660ef186cf04c5335b69c3a759
Mikael Brunnberg <mikael.brunnberg@gmail.com>
2016-04-04 17:05:03 +0200

    Bättre Navbar!

--- src/Inbetween/Models/KickAssDataBaseContext.cs
+++ src/Inbetween/Models/KickAssDataBaseContext.cs
@@ -9,21 +9,21 @@ namespace Inbetween.Models
     public class KickAssDataBaseContext : DbContext
     {
         //DB
-        public DbSet<News> News { get; set; }
-        public DbSet<Address> Addresses { get; set; }
-        public DbSet<Security> Securitys { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<ProductInOrder> ProductsInOrder { get; set; }
-        public DbSet<Product> Products { get; set; }
+        public DbSet<News> Inbetween_News { get; set; }
+        //public DbSet<Address> Addresses { get; set; }
+        //public DbSet<Security> Securitys { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        //public DbSet<ProductInOrder> ProductsInOrder { get; set; }
+        //public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>().ToTable("Users");
-            modelBuilder.Entity<Address>().ToTable("Addresses");
-            modelBuilder.Entity<Security>().ToTable("Securitys");
-            modelBuilder.Entity<Order>().ToTable("Orders");
-            modelBuilder.Entity<ProductInOrder>().ToTable("ProductsInOrder");
-            modelBuilder.Entity<Product>().ToTable("Products");
+            modelBuilder.Entity<News>().ToTable("Users");
+            //    modelBuilder.Entity<Address>().ToTable("Addresses");
+            //    modelBuilder.Entity<Security>().ToTable("Securitys");
+            //    modelBuilder.Entity<Order>().ToTable("Orders");
+            //    modelBuilder.Entity<ProductInOrder>().ToTable("ProductsInOrder");
+            //    modelBuilder.Entity<Product>().ToTable("Products");
         }
     }
 }
