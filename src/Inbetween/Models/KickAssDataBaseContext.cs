60ef42452c594638851b9d147fd12559e68164a9
Mikael Brunnberg <mikael.brunnberg@gmail.com>
2016-04-04 14:19:32 +0200

    Databaskoppling

---
+++ src/Inbetween/Models/KickAssDataBaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inbetween.Models
+{
+    public class KickAssDataBaseContext : DbContext
+    {
+        //DB
+        public DbSet<News> News { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Security> Securitys { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ProductInOrder> ProductsInOrder { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Address>().ToTable("Addresses");
+            modelBuilder.Entity<Security>().ToTable("Securitys");
+            modelBuilder.Entity<Order>().ToTable("Orders");
+            modelBuilder.Entity<ProductInOrder>().ToTable("ProductsInOrder");
+            modelBuilder.Entity<Product>().ToTable("Products");
+        }
+    }
+}
--- src/Inbetween/Startup.cs
+++ src/Inbetween/Startup.cs
@@ -7,8 +7,8 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Data.Entity;
-//using CatShelter.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Inbetween.Models;
 
 namespace Inbetween
 {
@@ -20,7 +20,14 @@ namespace Inbetween
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connString = @"Data Source=kickass.database.windows.net;Initial Catalog=KickAssDataBase;Integrated Security=False;User ID=kickass;Password=P@ssword;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            services.AddEntityFramework()
+            .AddSqlServer()
+            .AddDbContext<KickAssDataBaseContext>(o =>
+                o.UseSqlServer(connString));
             services.AddMvc();
+            services.AddCaching();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
