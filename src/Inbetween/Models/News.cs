ff07b38c69c916654c233f8a838749e794d399f2
Simon Llanso <cogliostro_g@hotmail.com>
2016-04-04 17:05:23 +0200

    Backend

---
+++ src/Inbetween/Models/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inbetween.Models
+{
+    public class News
+    {
+        public int UserId { get; set; }
+        public string Topic { get; set; }
+        public string Text { get; set; }
+        public image Image { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
