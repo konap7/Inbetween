13a482e49b297c0f6a836006127e87ef159e5a02
Mikael Brunnberg <mikael.brunnberg@gmail.com>
2016-04-05 16:00:59 +0200

    Login Changed

--- src/Inbetween/ViewModels/LoginVM.cs
+++ src/Inbetween/ViewModels/LoginVM.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +10,12 @@ namespace Inbetween.ViewModels
 {
     public class LoginVM
     {
+        [Display(Name = "User name")]
+        [Required(ErrorMessage = "You must enter a user name")]
         public string UserName { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "You must insert a password")]
         public string Password { get; set; }
     }
 }
